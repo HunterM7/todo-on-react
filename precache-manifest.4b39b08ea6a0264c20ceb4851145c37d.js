ebf91da1a5cf237184513d906c2ddfeb643b0a6c
Anton Meshchaninov <tonym7@yandex.ru>
2022-06-21 13:37:45 +0300

    Updates

---
+++ precache-manifest.4b39b08ea6a0264c20ceb4851145c37d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a263ab7f7d7ead1c463",
+    "url": "/todo-on-react/static/css/main.17da9f24.chunk.css"
+  },
+  {
+    "revision": "2a263ab7f7d7ead1c463",
+    "url": "/todo-on-react/static/js/main.2a263ab7.chunk.js"
+  },
+  {
+    "revision": "87bb8a3127dd8b7b3c9e",
+    "url": "/todo-on-react/static/js/1.87bb8a31.chunk.js"
+  },
+  {
+    "revision": "333478cf37e795e7386f",
+    "url": "/todo-on-react/static/js/runtime~main.333478cf.js"
+  },
+  {
+    "revision": "7e2d96820d1cb025d304bcc3d6ffcb1f",
+    "url": "/todo-on-react/index.html"
+  }
+];
